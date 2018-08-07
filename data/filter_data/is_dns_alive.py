906e5abe3eb0f1f556a4187e0c1f952f412d47b0
frenky-strasak <frenky.picasso@seznam.cz>
2018-08-07 16:56:27 +0200

    Better printing for dns alive.

--- data/filter_data/is_dns_alive.py
+++ data/filter_data/is_dns_alive.py
@@ -1,5 +1,6 @@
 import urllib.request
 import sys
+from time import time
 
 
 def save_new_dns_output(dns_list):
@@ -21,28 +22,37 @@ def check_dns_alive(url):
     # print(urllib.request.urlopen("http://google.com").getcode())
     if 'www.' not in url:
         url = 'www.' + url
-    print(url)
+
     try:
         # returned_code = urllib.request.urlopen('http://' + 'www.google.com').getcode()
         returned_code = urllib.request.urlopen('http://' + url).getcode()
     except:
-        return False
+        return False, url
+
     if returned_code == 200:
-        return True
-    return False
+        return True, url
+    return False, url
 
 
 def main(path):
     dns_list = []
     read_read_dns(path, dns_list)
-
     new_dns_list = []
-    for dns in dns_list:
-        if check_dns_alive(dns):
-            new_dns_list.append(dns)
-            print(dns)
+
+    t1 = time()
+
+    live_dns = 0
+    for i, dns in enumerate(dns_list):
+        is_alive, url = check_dns_alive(dns)
+        if is_alive:
+            new_dns_list.append(url)
+            print('{} {}    ==>     Connection was establihed. {}'.format(i, url, live_dns))
+            live_dns += 1
+        else:
+            print('{} {}    ==>     Connection was NOT establihed. {}'.format(i, url, live_dns))
 
     save_new_dns_output(new_dns_list)
+    print('Finished in {} hours'.format((time() - t1) / (60*60)))
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
